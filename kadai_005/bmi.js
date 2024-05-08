31a85ae20196bc2b1d5647e79f2b6713f833720b
shinboow <159513135+shinboow@users.noreply.github.com>
2024-05-08 13:06:29 +0900

    kadai_005 アップロード

--- kadai_005/bmi.js
+++ kadai_005/bmi.js
@@ -1,6 +1,21 @@
-let bmiValue;
+let bodyWeight;
 
-bmiValue = (68/(1.7*1.7));
+bodyWeight = ('体重');
 
+let weightValue = (68);
+
+let Height = ('身長');
+
+let heightValue = (1.7);
+
+let Bmi = ('BMI');
+
+let bmiValue = (68/(1.7*1.7));
+
+
+console.log (bodyWeight);
+console.log (weightValue);
+console.log (Height);
+console.log (heightValue);
+console.log (Bmi);
 console.log (bmiValue);
-
